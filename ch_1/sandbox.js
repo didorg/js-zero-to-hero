1ffe17259b7cfeec62b7b5a883b410271facb3b5
Didier <didorg@gmail.com>
2021-02-08 20:19:54 -0500

    Update sandbox.js

---
+++ ch_1/sandbox.js
@@ -0,0 +1,15 @@
+// common string methods
+
+let email = 'adam@javascript.com';
+
+//let result = email.lastIndexOf('p');
+
+//let result = email.slice(0,5);
+
+//let result = email.substr(5,12);
+
+//let result = email.replace('m', 'w');
+
+let result = email.replace('m', 'w');
+
+console.log(result);
