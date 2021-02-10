6215ceb4a6e63691d372b0ce2812f9592fa79c4f
Didier <didorg@gmail.com>
2021-02-09 19:00:08 -0500

    Update sandbox.js

--- ch_1/sandbox.js
+++ ch_1/sandbox.js
@@ -1,15 +1,41 @@
-// common string methods
-
+// --- common string methods ---
 let email = 'adam@javascript.com';
-
 //let result = email.lastIndexOf('p');
-
 //let result = email.slice(0,5);
-
 //let result = email.substr(5,12);
-
 //let result = email.replace('m', 'w');
+//let result = email.replace('m', 'w');
+/** ************************************************************* **/
 
-let result = email.replace('m', 'w');
+// --- numbers ---
+let radius = 10;
+let pi = 3.14;
+// console.log(radius, pi);
+// math operators  +, -, *, /, **, %
 
-console.log(result);
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of operation - B I D M A S
+// let result = 5 * (10 - 3)**2;
+// console.log(result);
+
+// shorthands
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes = likes + 10;
+// likes += 10;
+// likes *= 2;
+// likes /= 2;
+// console.log(likes);
+
+// NaN - not a number
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+//let result = 'the blog has ' + likes + ' likes.';
+//console.log(result);
+/** ************************************************************* **/
