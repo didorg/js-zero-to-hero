55c5b8c44668bf08ac3c88346401b05959df6cc7
Didier <didorg@gmail.com>
2021-02-09 20:39:20 -0500

    Update sandbox.js

---
+++ ch_2/sandbox.js
@@ -0,0 +1,30 @@
+// --- Control Flow --
+
+// --- for loops
+const names = ['shaun', 'mario', 'luigi'];
+
+for(let i = 0; i < names.length; i++){
+  // console.log(names[i]);
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+}
+
+// -- while loops
+//let i = 0;
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++;
+// }
+
+let i = 5;
+while(i > 2){
+  console.log('loop: ', i);
+  i--;
+}
+
+// -- do while loops
+//let i = 5
+do{
+  console.log('val of i is: ', i);
+  i++;
+} while(i < 5);
