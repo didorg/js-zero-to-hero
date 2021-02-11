5274460f916df473b7001ff0d2146db12032d416
Didier <didorg@gmail.com>
2021-02-10 19:45:37 -0500

    chapter 3 function declarations & Expressions

---
+++ ch_3/sandbox.js
@@ -0,0 +1,17 @@
+// --- function declarations &  Expressions 
+
+
+// function declaration
+function greet(){
+  console.log('hello there');
+}
+
+// function expression
+const speak = function(){
+  console.log('good day!');
+};
+
+// greet();
+// greet();
+
+speak();
