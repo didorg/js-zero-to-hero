bfbe92f14e6b47929499ad9857737a6f887fecf5
Didier <didorg@gmail.com>
2021-02-10 19:27:13 -0500

    Update sandbox.js

--- ch_2/sandbox.js
+++ ch_2/sandbox.js
@@ -28,3 +28,37 @@ do{
   console.log('val of i is: ', i);
   i++;
 } while(i < 5);
+
+// --- else if statements
+const password = 'p@ssword123456';
+
+if(password.length >= 12){
+  console.log('that password is mighty strong');
+} else if(password.length >= 8){
+  console.log('that password is long enough');
+} else {
+  console.log('password should be at least 8 characters long');
+}
+
+// --- switch statements ---
+const grade = 'D';
+
+switch(grade){
+  case 'A':
+    console.log('you got an A!');
+    break;
+  case 'B':
+    console.log('you got a B!');
+    break;
+  case 'C':
+    console.log('you got a C!');
+    break;
+  case 'D':
+    console.log('you got a D!');
+    break;
+  case 'E':
+    console.log('you got an E!');
+    break;
+  default:
+    console.log('not a valid grade');
+}
