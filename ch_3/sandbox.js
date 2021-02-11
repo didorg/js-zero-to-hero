c132115d5b3837093f8adfc07afd34969820d0e0
Didier <didorg@gmail.com>
2021-02-10 20:53:18 -0500

    Update sandbox.js

--- ch_3/sandbox.js
+++ ch_3/sandbox.js
@@ -1,17 +1,65 @@
 // --- function declarations &  Expressions 
 
-
 // function declaration
-function greet(){
+/* function greet(){
   console.log('hello there');
-}
+} */
 
 // function expression
-const speak = function(){
+//Function expressions in JavaScript are not hoisted, unlike function declarations. You can't use function expressions before you create them
+
+/* const speak = function(){
   console.log('good day!');
+}; */
+// greet();
+// greet();
+//speak();
+
+// arguments & parameters
+const speak = function(name = 'luigi', time = 'night'){
+  console.log(`good ${time}, ${name}!`);
 };
 
-// greet();
-// greet();
+// speak('mario', 'morning');
+// speak();
+speak('shaun');
 
-speak();
+/************************  Arrow Functions ***********************/
+
+// regular function
+
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// }
+
+// arrow function
+const calcArea = radius => 3.14 * radius**2;
+
+const area = calcArea(5);
+console.log('area is:', area);
+
+// practise arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
+// }
+const greet = () => 'hello, world';
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i]  + products[i] * tax;
+  }
+  return total;
+}
+
+console.log(greet());
+console.log(bill([10,15,30], 0.2));
+/************************  **************** ***********************/
